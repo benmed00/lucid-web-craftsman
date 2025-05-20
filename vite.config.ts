a74e71ebfd9035e9ea32457184d2549a1f43f0c0
Jacob Son <ben94med@gmail.com>
2025-05-20 03:34:54 +0200

    deployement config to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/lucid-web-craftsman/", // Remplace par le nom de ton repo GitHub
   server: {
     host: "::",
     port: 8080,
